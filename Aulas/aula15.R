98991bb87608443527ad600d3e51d7f06033805a
shenrique08 <sergimmatos@hotmail.com>
2024-10-25 10:52:01 -0300

    aula15

---
+++ Aulas/aula15.R
@@ -0,0 +1,28 @@
+dados <- read.csv("grilo.txt", header = TRUE, sep = ",")
+str(dados)
+
+library(ggplot2)
+ggplot(data = dados, aes(x = frequencia))+geom_histogram(bins = 10)
+
+
+cor(dados)
+
+ggplot(data = dados, aes(x = temperatura, y = frequencia))+geom_point()+theme_minimal()+geom_smooth(method = "lm")
+
+modelo_linear <- lm(formula = frequencia ~ temperatura, data = dados)
+
+modelo_linear
+summary(dados$temperatura)
+
+w <- data.frame(temperatura = c(21, 23.6, 30.9))
+
+predict(modelo_linear, newdata = w)
+library(palmerpenguins)
+cor(dados$bill_length_mm, dados$bill_depth_mm)
+summary(dados)
+
+dados <- na.omit(dados)
+names(dados)
+cor(dados[, 3:6])
+
+ggplot(data = dados, aes(x = flipper_length_mm, y = body_mass_g, color = species))+geom_point()+geom_smooth(method = "lm")
